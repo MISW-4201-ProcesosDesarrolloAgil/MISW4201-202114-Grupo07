96cbe5deb3f386413cfa88917c8f28bad9613583
Daniel <daniquice.developer@gmail.com>
2021-09-11 21:38:34 -0500

    DMAG-62,64

--- front-end/tutorial-canciones/src/app/album/album-comment/album-comment.component.spec.ts
+++ front-end/tutorial-canciones/src/app/album/album-comment/album-comment.component.spec.ts
@@ -5,13 +5,28 @@ import { DebugElement } from '@angular/core';
 
 import { AlbumCommentComponent } from './album-comment.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { RouterTestingModule } from '@angular/router/testing';
+
+
 describe('AlbumCommentComponent', () => {
   let component: AlbumCommentComponent;
   let fixture: ComponentFixture<AlbumCommentComponent>;
+  let debug: DebugElement;
+  const formBuilder: FormBuilder = new FormBuilder();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlbumCommentComponent ]
+        imports: [HttpClientModule, ReactiveFormsModule,
+        FormsModule, ToastrModule.forRoot(), RouterTestingModule],
+      declarations: [ AlbumCommentComponent ],
+      providers: [
+
+        { provide: FormBuilder, useValue: formBuilder }
+      ]
     })
     .compileComponents();
   }));
@@ -19,7 +34,11 @@ describe('AlbumCommentComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AlbumCommentComponent);
     component = fixture.componentInstance;
+    component.albumCommentForm = formBuilder.group({
+      comentario: null
+    });
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
